15943778f8068a2ba11b87c9b5ad66b17102c896
Tobias Gjerstrup <tobiasgjerstrup@gmail.com>
2025-07-21 20:47:02 +0200

    0.0.12 Added explaination to benchmark errors

--- cmd/benchmark/main.go
+++ cmd/benchmark/main.go
@@ -3,13 +3,12 @@ package main
 import "rolerocket/internal/benchmark"
 
 func main() {
-	benchmark.RunBenchmark("http://localhost:8080/users", 100, 5)
+	benchmark.RunBenchmark("http://localhost:8080/users", 10000, 5)
 }
 
 /*
-commands to get list of all active things
+commands to get list of all active things. Failures will start appearing at around 40-50k open connections on windows by default
 
 netsh int ipv4 show dynamicport tcp
 netstat -an | Select-String ":8080" | Group-Object { ($_ -split '\s+')[-1] } | Format-Table Count, Name
-
 */
