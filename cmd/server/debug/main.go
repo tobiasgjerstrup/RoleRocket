5d7d21f25f6fb6ebda30a37fc212043c15b56609
Tobias Gjerstrup <tobiasgjerstrup@gmail.com>
2025-07-26 13:58:16 +0200

    0.0.18 Added a debug mode

--- cmd/server/debug/main.go
+++ cmd/server/debug/main.go
@@ -9,13 +9,13 @@ import (
 )
 
 func main() {
-	router := routes.Routes(false)
+	router := routes.Routes(true)
 	db := sqlite.Init()
 	logger.Main(db)
 
 	// ? use localhost:xxxx to make it not ask for admin permissions
 	// ? use :8080 for production and benchmarking! This changes how TCP behaves so it's very important to use :8080
-	port := ":8080"
+	port := "localhost:8080"
 	logger.Info(context.Background(), "Starting server on port: "+port)
 	server := http.Server{
 		Addr:    port,
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,9 +3,10 @@ package routes
 import (
 	"encoding/json"
 	"net/http"
+	sqlite "rolerocket/internal/db"
 )
 
-func Routes() *http.ServeMux {
+func Routes(debugMode bool) *http.ServeMux {
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /users", func(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +39,62 @@ func Routes() *http.ServeMux {
 	router.HandleFunc("POST /users/token", func(w http.ResponseWriter, r *http.Request) {
 		GetToken(w, r)
 	})
+
+	if debugMode {
+		router.HandleFunc("GET /debug/query/{query}", func(w http.ResponseWriter, r *http.Request) {
+			query := r.PathValue("query")
+
+			rows, err := sqlite.DBInstance.Conn.Query(query)
+			if err != nil {
+				http.Error(w, "Failed running debug query", http.StatusInternalServerError)
+				return
+			}
+			defer rows.Close()
+
+			columns, err := rows.Columns()
+			if err != nil {
+				http.Error(w, "Failed to get columns", http.StatusInternalServerError)
+				return
+			}
+
+			var results []map[string]interface{}
+
+			for rows.Next() {
+				values := make([]interface{}, len(columns))
+				valuePtrs := make([]interface{}, len(columns))
+
+				for i := range columns {
+					valuePtrs[i] = &values[i]
+				}
+
+				if err := rows.Scan(valuePtrs...); err != nil {
+					http.Error(w, "Error scanning row", http.StatusInternalServerError)
+					return
+				}
+
+				rowMap := make(map[string]interface{})
+				for i, col := range columns {
+					val := values[i]
+
+					// Handle []byte as string
+					if b, ok := val.([]byte); ok {
+						rowMap[col] = string(b)
+					} else {
+						rowMap[col] = val
+					}
+				}
+				results = append(results, rowMap)
+			}
+
+			jsonBytes, err := json.Marshal(results)
+			if err != nil {
+				http.Error(w, "Failed to encode result", http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonBytes)
+		})
+	}
 
 	return router
 }
